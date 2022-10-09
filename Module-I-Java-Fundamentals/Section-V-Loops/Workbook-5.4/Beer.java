c31716baa404ec18e3f43bd4dc152bd9a60e9b40
emtaylor1993 <etaylorsubs1993@gmail.com>
2022-10-09 13:28:19 -0400

    + Added code for Workbook 5.4

---
+++ Module-I-Java-Fundamentals/Section-V-Loops/Workbook-5.4/Beer.java
@@ -0,0 +1,22 @@
+public class Beer {
+    public static void main(String[] args) {
+        for (int i = 99; i >= 1; i--) {
+            sing(i);
+        }
+    }
+
+    /** Task 1
+     * Function name – sing.
+     * @param i (int)
+     * 
+     * 
+     * Inside the function: 
+     *  1. print:
+     *      <i> bottles of beer on the wall, <i> bottles of beer!
+     *      take one down, pass it around, <i-1> bottles of beer on the wall!
+     */
+    public static void sing(int i) {
+        System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer!");
+        System.out.println("take one down, pass it around, " + (i - 1) + " bottles of beer on the wall!");
+    }
+}
