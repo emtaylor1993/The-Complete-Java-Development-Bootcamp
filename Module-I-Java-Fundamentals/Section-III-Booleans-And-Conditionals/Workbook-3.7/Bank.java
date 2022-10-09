48478e9575681b5f83eb35975f13ab0702b98744
emtaylor1993 <etaylorsubs1993@gmail.com>
2022-10-09 11:15:05 -0400

    + Added code for Workbook 3.7

---
+++ Module-I-Java-Fundamentals/Section-III-Booleans-And-Conditionals/Workbook-3.7/Bank.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Bank {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\n****ROYAL BANK OF JAVA****");
+        System.out.println("Are you here to get a mortgage? (yes or no)");
+        
+        String choice = scan.next();
+
+        if (choice.equals("yes")) {
+            System.out.println("\nGreat! In one line" +
+                "\nHow much money do you have in your savings?" +
+                "\nAnd, how much do you owe in credit card debt?");
+            int savings = scan.nextInt();
+            int debt = scan.nextInt();
+
+            System.out.println("\nHow many years have you worked for?");
+            int years = scan.nextInt();
+
+            System.out.println("\nWhat is your name?");
+            String name = scan.next();
+
+            if (savings >= 10000 && debt < 5000 && years >= 2) {
+                System.out.println("\nCongratulations " + name + "! You have been approved!");
+            } else {
+                System.out.println("\nSorry, you are not eligible for a mortgage.");
+            }
+        } else {
+            System.out.println("\nOK. Have a nice day!");
+        }
+
+        scan.close();
+    }
+}
