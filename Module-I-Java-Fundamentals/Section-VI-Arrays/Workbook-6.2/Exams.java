9ecc7757ebb58f1f9a3d2c90d349b71a7f07bb96
emtaylor1993 <etaylorsubs1993@gmail.com>
2022-10-10 10:28:20 -0400

    + Added code for Workbook 6.2

---
+++ Module-I-Java-Fundamentals/Section-VI-Arrays/Workbook-6.2/Exams.java
@@ -0,0 +1,10 @@
+public class Exams {
+    public static void main(String[] args) {
+        System.out.println("It's time to take your 5th years exams. Please, take your seats.\n");
+        String[] students = {"Malfoy", "Crabbe", "Goyle", "Pansy", "Dean"};
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i] + ", you will take seat " + i);
+        }
+    }
+}
